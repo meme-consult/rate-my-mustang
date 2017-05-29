b533b1ee7a8496246634dd6384358a6e38208940
Killian McGuinness <kmcguin@uwo.ca>
2017-05-29 19:16:43 +0000

    Add display of ratings to prof table

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function addRatingsButtonToProfCell() {
 }
 
 function handleRatingRequest() {
-
+  const that = this;
   // Search for a prof at Western to get a list of RMP search results
   $.ajax({
     url: this.query
@@ -41,6 +41,35 @@ function handleRatingRequest() {
         difficulty: $(response).find('.difficulty .grade')[0].innerText.trim()
       };
 
+      createRatingsDisplay(prof, that);
     });
   });
+};
+
+function createRatingsDisplay(profData, element) {
+  console.log(profData);
+  let ratings = document.createElement('div');
+  $(ratings).addClass('ratings');
+
+  let quality = document.createElement('div');
+  $(quality).attr('id', 'quality');
+  quality.innerHTML = '<h5>Quality</h5>';
+  let qualityGrade = document.createElement('span');
+  $(qualityGrade).addClass('grade');
+  qualityGrade.innerHTML = profData.quality;
+  quality.appendChild(qualityGrade);
+
+  let difficulty = document.createElement('div');
+  $(difficulty).attr('id', 'difficulty');
+  difficulty.innerHTML = '<h5>Difficulty</h5>';
+  let difficultyGrade = document.createElement('span');
+  $(difficultyGrade).addClass('grade');
+  difficultyGrade.innerHTML = profData.difficulty;
+  difficulty.appendChild(difficultyGrade);
+
+  element.appendChild(ratings);
+
+  ratings.appendChild(quality);
+  ratings.appendChild(difficulty);
+  console.log(element);
 };
